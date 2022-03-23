d496e94c1f7cb53c959c497a8b4d174d6e6e5b1a
Archdemon32 <102227300+Archdemon32@users.noreply.github.com>
2022-03-23 21:37:11 +0000

    Add files via upload

---
+++ dash3.py
@@ -0,0 +1,74 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import numpy as np
+import calendar
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import datamodel
+
+order = datamodel.get_data()
+df_year = datamodel.get_year()
+df_month = datamodel.get_month()
+
+fig_employee = px.bar(order,
+    x='emp_name', y='total',
+    color='type', text='total', title='Sales by Employee',
+    hover_data=[],
+    labels={'total':'Total sales', 'emp_name':'Employee', 'type':'Product Type'})
+fig_employee.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
+app = dash.Dash(__name__)
+dash_app = dash.Dash(__name__)
+app = dash_app.server
+
+dash_app.layout = html.Div(
+    children=[
+        html.Div(className='row',
+                children=[
+                    html.Div(className='four columns div-user-controls',
+                            children=[
+                                html.H2('Sales dashboard'),
+                                html.P('Select filters from dropdown'),
+
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month',
+                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year',
+                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]),]),
+                    html.Div(className='eight columns div-for-charts bg-grey',
+                            children=[
+                                dcc.Graph(id="sales_employee", figure=fig_employee)]),])])
+
+
+@dash_app.callback(Output('sales_employee', 'figure'),
+              [Input('drop_month', 'value')],
+              [Input('drop_year', 'value')])
+
+def update_graph(drop_month, drop_year):
+    if drop_year:
+        if drop_month:
+            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
+        else:
+            order_fig1 = order.loc[order['orderyear'] == drop_year]
+    else:
+        if drop_month:
+            order_fig1 = order.loc[order['ordermonth'] == drop_month]
+        else:
+            order_fig1 = order
+
+    return {'data':[go.Bar(
+        x = order_fig1['productname'],
+        y = order_fig1['total'])]}
+
+if __name__ == '__main__':
+    dash_app.run_server(debug=True)
